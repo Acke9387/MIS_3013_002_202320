8f38c1b0f5cf9f5005a59f9bf38236a3ec9e7129
Ackerman, Adam P. <adam@ou.edu>
2024-02-13 17:33:05 -0600

    Did an inclass participation over string manipulation

---
+++ In_Class_Examples/StringManipulation/Program.cs
@@ -0,0 +1,20 @@
+﻿//             012345678910
+string name = "hENRY JAMES";
+//H.J.
+string name2 = "Clark Kent";
+//C.K.
+
+string lowerName = name.ToLower();
+
+Console.WriteLine(lowerName);
+
+string firstLetterOfFirstName = name.Substring(0, 1).ToUpper();
+//using index notation
+//firstLetterOfFirstName = name[0].ToString();
+//alternative way
+//firstLetterOfFirstName = firstLetterOfFirstName.ToUpper();
+int indexOfSpace = name.IndexOf(' ');
+string firstLetterOfSecondName = name.Substring(indexOfSpace + 1, 1);
+
+string initials = $"{firstLetterOfFirstName}.{firstLetterOfSecondName}.";
+Console.WriteLine(initials);
