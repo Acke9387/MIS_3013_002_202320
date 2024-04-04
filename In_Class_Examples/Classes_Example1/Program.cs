a02f8f98b60d1f09e5f0c73996d86773c2047841
Ackerman, Adam P. <adam@ou.edu>
2024-04-04 17:41:41 -0500

    Completed in class example over classes

---
+++ In_Class_Examples/Classes_Example1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Classes_Example1;
+
+Rectangle r = new Rectangle();
+r.Width = 5;
+r.Length = 4;
+
+Rectangle r1 = new Rectangle();
+r1.Width = 90;
+r1.Length = 2;
+//r1.Message = "";
+
+Console.WriteLine($"The area of rectangle r is {r.CalculateArea()}");
+
+double r1Perimeter = r1.CalculatePerimeter();
+
+Console.WriteLine($"The area of rectangle r1 is {r1Perimeter}");
---
+++ In_Class_Examples/Classes_Example1/Rectangle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Example1
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Rectangle
+    {
+        /// <summary>
+        /// The Length value of the Rectangle
+        /// </summary>
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        private string Message;
+
+        /// <summary>
+        /// Default/Empty Constructor
+        /// </summary>
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+            Message = string.Empty;
+        }
+
+        /// <summary>
+        /// Calculate the Area of the Rectangle
+        /// </summary>
+        /// <returns>The area of the Rectangle</returns>
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+    }
+}
